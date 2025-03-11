e5e2398d077b02ac35a7057bc5025b45f4cc9291
Sushi <71588514+projetuser@users.noreply.github.com>
2025-03-11 15:01:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,6 +8,8 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
+from flask_jwt_extended import get_jwt
+from datetime import timedelta, datetime
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
