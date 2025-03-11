c4029663559c27b2d096f262cbf10c30884a7273
Sushi <71588514+projetuser@users.noreply.github.com>
2025-03-11 13:56:09 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -41,4 +41,4 @@ def protected():
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
-
+ 
